62d07ee94c13ed7d475e29cb169d556fbb89233a
Josh-16s <joshuaadekola123@gmail.com>
2025-07-14 13:05:57 +0100

    changed settings file

--- backend/settings.py
+++ backend/settings.py
@@ -72,6 +72,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CORS_ALLOWED_ORIGINS = [
     "https://hemonisyfood.com",
     "http://hemonisyfood.com",  
+    "http://hemonisyfood.com/",
 ]
 
 REST_FRAMEWORK = {
